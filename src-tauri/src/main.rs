1a434f6069fd8f037d3db23a673d832ed8834aff
ayeareem <ag.ayeareem@gmail.com>
2023-07-20 05:16:35 +0800

    forgot to remove unused crate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::process::Command;
-
 use tauri::Size;
 use tauri::Window;
 use walkdir::WalkDir;
